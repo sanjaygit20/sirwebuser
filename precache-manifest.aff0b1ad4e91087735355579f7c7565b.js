ec1b9393cb4a01a0241cd5badf2f70fecc3f8ff8
sanjay <sanjay.igit14@gmail.com>
2020-09-27 15:55:06 +0530

    Updates

---
+++ precache-manifest.aff0b1ad4e91087735355579f7c7565b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9474a46f42eb0a676d350506cfaf829f",
+    "url": "/sirwebuser/index.html"
+  },
+  {
+    "revision": "08ff35b619c2247f05b3",
+    "url": "/sirwebuser/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "8ced6a8bd573577a0942",
+    "url": "/sirwebuser/static/css/main.1aefd48c.chunk.css"
+  },
+  {
+    "revision": "08ff35b619c2247f05b3",
+    "url": "/sirwebuser/static/js/2.6d554cab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sirwebuser/static/js/2.6d554cab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ced6a8bd573577a0942",
+    "url": "/sirwebuser/static/js/main.d08ab9c7.chunk.js"
+  },
+  {
+    "revision": "32a145cf410b77c0ad18",
+    "url": "/sirwebuser/static/js/runtime-main.b5749dd6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sirwebuser/static/media/logo.5d5d9eef.svg"
+  }
+]);
